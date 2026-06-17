1e0bfdffcb36cbbad823bafed9b71c1a1fad00e9
sai5451 <sindiri.saisasi@gmail.com>
2026-06-17 14:50:53 +0530

    added expense fields

--- app/src/main/java/com/prespend/models/Expense.java
+++ app/src/main/java/com/prespend/models/Expense.java
@@ -2,4 +2,12 @@ package com.prespend.models;
 
 public class Expense {
 
+
+    private int id;
+    private double amount;
+    private String category;
+    private String paymentMode;
+    private String merchant;
+    private String timestamp;
+    private String notes;
 }
