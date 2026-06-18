3dc41576f1ecadf30cf33c5d4f0debac37abacf4
sai5451 <sindiri.saisasi@gmail.com>
2026-06-18 13:12:24 +0530

    Extended DatabaseHelper with SQLiteOpenHelper

--- app/src/main/java/com/prespend/database/DatabaseHelper.java
+++ app/src/main/java/com/prespend/database/DatabaseHelper.java
@@ -1,6 +1,18 @@
 package com.prespend.database;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+public class DatabaseHelper extends SQLiteOpenHelper {
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
