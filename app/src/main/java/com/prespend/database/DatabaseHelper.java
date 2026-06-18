d96fcee2597bd7d55ec918e0cc215904348f1825
sai5451 <sindiri.saisasi@gmail.com>
2026-06-18 13:14:03 +0530

    Added database configuration and constructor

--- app/src/main/java/com/prespend/database/DatabaseHelper.java
+++ app/src/main/java/com/prespend/database/DatabaseHelper.java
@@ -6,6 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "prespend.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
